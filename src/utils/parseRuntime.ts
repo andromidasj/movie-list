4e60280e3b6e19e1be244eee8d709856a1f6f1c9
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2023-10-23 17:02:18 -0500

    fix duration parsing

--- src/utils/parseRuntime.ts
+++ src/utils/parseRuntime.ts
@@ -1,9 +1,8 @@
 export function parseRuntime(duration = 0) {
   if (duration === 0) return null;
 
-  const runtimeMinutes = Math.floor(duration / 1000 / 60);
-  const hours = Math.floor(runtimeMinutes / 60);
-  const minutes = runtimeMinutes % 60;
+  const hours = Math.floor(duration / 60);
+  const minutes = duration % 60;
 
   const hoursStr = !!hours ? `${hours} hr` : null;
   const minutesStr = !!minutes ? `${minutes} min` : null;
